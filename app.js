0d20d57029e4f3fd4d0191d389c93d876dcba7df
Kenneth Yu <kenyu@ucdavis.edu>
2014-04-18 19:03:29 -0700

    user can post in chat server
    * message gets broadcast to other sockets

--- app.js
+++ app.js
@@ -10,4 +10,15 @@ server.on('connection', function(socket) {
   sockets.push(socket)
 
   socket.write('Hello and welcome to PandaChat!\n')
+
+  socket.on('data', function(data) {
+    for (var i = 0; i < sockets.length; i ++) {
+      if (sockets[i] !== this) { sockets[i].write(data) }
+    }
+  })
+
+  socket.on('end', function() {
+    var i = sockets.indexOf(this)
+    sockets.splice(i, 1)
+  })
 })
